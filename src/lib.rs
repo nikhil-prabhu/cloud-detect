5efe9f09889ac4db0fabe926fa290d2829116f50
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-08-07 22:48:56 +0530

    feat: Add fallible detection capability

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 mod consts;
 
 use std::collections::HashMap;
+use std::error::Error;
 use std::sync::mpsc::{self, TryRecvError};
 use std::time::{Duration, Instant};
 
@@ -49,7 +50,7 @@ pub fn supported_providers() -> Vec<&'static str> {
 
 // TODO: add test(s)
 /// Detects the current host's cloud service provider.
-/// Returns "unknown" if the detection failed, or if the current cloud service provider is unsupported.
+/// Returns "unknown" if the detection failed, if the current cloud service provider is unsupported, or if minor errors occurred during detection.
 ///
 /// # Arguments
 ///
@@ -85,4 +86,48 @@ pub async fn detect(timeout: Option<u64>) -> String {
     };
 
     provider
+}
+
+// TODO: add test(s)
+/// Attempts to detect the current host's cloud service provider.
+/// If we encounter an error, we return it rather than unwrapping or assuming the provider as "unknown".
+///
+/// **NOTE**: This also means that this function returns an error if the current host's provider is unsupported.
+///
+/// # Arguments
+///
+/// * `timeout` - How long to attempt detection for (in seconds). Defaults to 3 seconds.
+pub async fn try_detect(timeout: Option<u64>) -> Result<String, Box<dyn Error>> {
+    // Set default timeout if none specified.
+    let timeout_duration = Duration::from_secs(timeout.unwrap_or(DETECTION_TIMEOUT));
+
+    // Concurrently check if the current host belongs to any of the supported providers and write the detected provider
+    // to a channel.
+    let (tx, rx) = mpsc::sync_channel::<String>(1);
+    for (provider, metadata_url) in PROVIDER_METADATA_MAP.iter() {
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            if ping(metadata_url).await {
+                tx.send(provider.to_string())?;
+            }
+
+            Ok::<(), Box<dyn Error + Send + Sync>>(())
+        });
+    }
+
+    // Wait for a value from the channel or timeout.
+    let start_time = Instant::now();
+    let provider = loop {
+        match rx.try_recv() {
+            Ok(value) => break Ok(value),
+            Err(TryRecvError::Empty) => {
+                if start_time.elapsed() >= timeout_duration {
+                    break Err("Timed out when attempting to detect provider".to_string())?;
+                }
+            }
+            Err(err) => break Err(err),
+        }
+    }?;
+
+    Ok(provider)
 }
