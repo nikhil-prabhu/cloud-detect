a31bd425292df38f4d0e702015d63db80d241ca2
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-08-08 20:16:24 +0530

    feat: Add non-async functionality

--- src/blocking/mod.rs
+++ src/blocking/mod.rs
@@ -1,64 +1,31 @@
-///! Detect a host's cloud service provider.
-mod consts;
-
-#[cfg(feature = "blocking")]
-pub mod blocking;
-
-use std::collections::HashMap;
+///! Detects a host's cloud service provider in a blocking manner.
 use std::error::Error;
 use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
 use std::time::{Duration, Instant};
 
-use lazy_static::lazy_static;
+use crate::{DETECTION_TIMEOUT, PROVIDER_METADATA_MAP};
 
-use consts::*;
-
-lazy_static! {
-    /// A mapping of supported cloud providers with their metadata URLs.
-    pub(crate) static ref PROVIDER_METADATA_MAP: HashMap<&'static str, &'static str> = {
-        let mut map = HashMap::new();
-        map.insert(AMAZON_WEB_SERVICES, "http://169.254.169.254/latest/");
-        map.insert(
-            MICROSOFT_AZURE,
-            "http://169.254.169.254/metadata/v1/InstanceInfo",
-        );
-        map.insert(
-            GOOGLE_CLOUD_PLATFORM,
-            "http://metadata.google.internal/computeMetadata/",
-        );
-        map
-    };
-}
-
-/// Makes a GET request to the specified metadata URL and returns true if successful.
+/// Makes a blocking GET request to the specified metadata URL and returns true if successful.
 ///
 /// # Arguments
 ///
 /// * `metadata_url` - The metadata URL for the cloud service provider.
-async fn ping(metadata_url: &str) -> bool {
-    match reqwest::get(metadata_url).await {
+fn ping(metadata_url: &str) -> bool {
+    match reqwest::blocking::get(metadata_url) {
         Ok(resp) => resp.status() == reqwest::StatusCode::OK,
         Err(_) => false,
     }
 }
 
 // TODO: add test(s)
-/// Returns a list of the currently supported cloud service providers.
-pub fn supported_providers() -> Vec<&'static str> {
-    PROVIDER_METADATA_MAP
-        .keys()
-        .copied()
-        .collect::<Vec<&'static str>>()
-}
-
-// TODO: add test(s)
-/// Detects the current host's cloud service provider.
+/// Detects the current host's cloud service provider in a blocking manner.
 /// Returns "unknown" if the detection failed, if the current cloud service provider is unsupported, or if minor errors occurred during detection.
 ///
 /// # Arguments
 ///
 /// * `timeout` - How long to attempt detection for (in seconds). Defaults to 3 seconds.
-pub async fn detect(timeout: Option<u64>) -> String {
+pub fn detect(timeout: Option<u64>) -> String {
     // Set default timeout if none specified.
     let timeout_duration = Duration::from_secs(timeout.unwrap_or(DETECTION_TIMEOUT));
 
@@ -67,8 +34,8 @@ pub async fn detect(timeout: Option<u64>) -> String {
     let (tx, rx) = mpsc::sync_channel::<String>(1);
     for (provider, metadata_url) in PROVIDER_METADATA_MAP.iter() {
         let tx = tx.clone();
-        tokio::spawn(async move {
-            if ping(metadata_url).await {
+        thread::spawn(move || {
+            if ping(metadata_url) {
                 tx.send(provider.to_string()).unwrap();
             }
         });
@@ -92,7 +59,7 @@ pub async fn detect(timeout: Option<u64>) -> String {
 }
 
 // TODO: add test(s)
-/// Attempts to detect the current host's cloud service provider.
+/// Attempts to detect the current host's cloud service provider in a blocking manner.
 /// If we encounter an error, we return it rather than unwrapping or assuming the provider as "unknown".
 ///
 /// **NOTE**: This also means that this function returns an error if the current host's provider is unsupported.
@@ -100,7 +67,7 @@ pub async fn detect(timeout: Option<u64>) -> String {
 /// # Arguments
 ///
 /// * `timeout` - How long to attempt detection for (in seconds). Defaults to 3 seconds.
-pub async fn try_detect(timeout: Option<u64>) -> Result<String, Box<dyn Error>> {
+pub fn try_detect(timeout: Option<u64>) -> Result<String, Box<dyn Error>> {
     // Set default timeout if none specified.
     let timeout_duration = Duration::from_secs(timeout.unwrap_or(DETECTION_TIMEOUT));
 
@@ -109,8 +76,8 @@ pub async fn try_detect(timeout: Option<u64>) -> Result<String, Box<dyn Error>> 
     let (tx, rx) = mpsc::sync_channel::<String>(1);
     for (provider, metadata_url) in PROVIDER_METADATA_MAP.iter() {
         let tx = tx.clone();
-        tokio::spawn(async move {
-            if ping(metadata_url).await {
+        thread::spawn(move || {
+            if ping(metadata_url) {
                 tx.send(provider.to_string())?;
             }
 
