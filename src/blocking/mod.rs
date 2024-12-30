700eb9e703d0a2f0e69e20180fd14c48b9459a77
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-12-30 15:10:09 +0530

    chore: define base module structure

--- src/blocking/mod.rs
+++ src/blocking/mod.rs
@@ -1,1 +1,26 @@
+use std::sync::mpsc::Sender;
+use std::sync::{LazyLock, Mutex};
+use std::time::Duration;
 
+use anyhow::Result;
+
+use crate::{ProviderId, P};
+
+/// Represents a cloud service provider.
+#[allow(dead_code)]
+pub(crate) trait Provider: Send + Sync {
+    fn identifier(&self) -> ProviderId;
+    fn identify(&self, tx: Sender<ProviderId>, timeout: Duration);
+}
+
+#[allow(dead_code)]
+static PROVIDERS: LazyLock<Mutex<Vec<P>>> = LazyLock::new(|| todo!());
+
+pub fn supported_providers() -> Result<ProviderId> {
+    todo!()
+}
+
+#[allow(unused_variables)]
+pub fn detect(timeout: Option<u64>) -> Result<ProviderId> {
+    todo!()
+}
